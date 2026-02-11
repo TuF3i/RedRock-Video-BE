b254b259626d0a2ead745df0ad144ac26b4bb824
TuF3i <13759301652@163.com>
2026-02-11 12:24:27 +0800

    ⚡️ 使用泛型封装Kitex的响应生成函数

--- apps/rpc/videosvr/core/dto/kitex-dto.go
+++ apps/rpc/videosvr/core/dto/kitex-dto.go
@@ -4,11 +4,48 @@ import "LiveDanmu/apps/rpc/videosvr/kitex_gen/videosvr"
 
 // DtoResp 接口
 type DtoResp interface {
-	GetStatus()
-	GetInfo()
+	GetStatus() int64
+	GetInfo() string
 }
 
-// KitexResponse泛型接口
+// KitexResp KitexResponse泛型接口
 type KitexResp interface {
 	*videosvr.AddVideoResp | *videosvr.DelVideoResp | *videosvr.JudgeAccessResp | *videosvr.GetVideoListResp | *videosvr.GetPreSignedUrlResp
+}
+
+// GenKitexResp 生成Kitex响应
+func GenKitexResp[T KitexResp](resp DtoResp, data interface{}) T {
+	// 声明T的实例
+	var res T
+	// 类型推断
+	switch v := any(res).(type) {
+	case *videosvr.AddVideoResp:
+		v = new(videosvr.AddVideoResp)
+		v.SetStatus(resp.GetStatus())
+		v.SetInfo(resp.GetInfo())
+		res = any(v).(T)
+	case *videosvr.DelVideoResp:
+		v = new(videosvr.DelVideoResp)
+		v.SetStatus(resp.GetStatus())
+		v.SetInfo(resp.GetInfo())
+		res = any(v).(T)
+	case *videosvr.JudgeAccessResp:
+		v = new(videosvr.JudgeAccessResp)
+		v.SetStatus(resp.GetStatus())
+		v.SetInfo(resp.GetInfo())
+		res = any(v).(T)
+	case *videosvr.GetVideoListResp:
+		v = new(videosvr.GetVideoListResp)
+		v.SetStatus(resp.GetStatus())
+		v.SetInfo(resp.GetInfo())
+		v.SetData(data.(*videosvr.GetVideoListData))
+		res = any(v).(T)
+	case *videosvr.GetPreSignedUrlResp:
+		v = new(videosvr.GetPreSignedUrlResp)
+		v.SetStatus(resp.GetStatus())
+		v.SetInfo(resp.GetInfo())
+		v.SetData(data.(string))
+		res = any(v).(T)
+	}
+	return res
 }
