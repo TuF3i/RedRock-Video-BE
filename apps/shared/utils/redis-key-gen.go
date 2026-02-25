4758305bbe771728588a7702bf111f7e76b9fb65
TuF3i <13759301652@163.com>
2026-02-25 18:55:26 +0800

    🐛 修复事务管道中的CROSSSLOT 错误

--- apps/shared/utils/redis-key-gen.go
+++ apps/shared/utils/redis-key-gen.go
@@ -5,11 +5,11 @@ import (
 )
 
 func GenHotDanmuKey(vid int64) string {
-	return fmt.Sprintf("dm:data:hot:%v", vid)
+	return fmt.Sprintf("dm:data:hot:{%v}", vid)
 }
 
 func GenFullDanmuKey(vid int64) string {
-	return fmt.Sprintf("dm:data:full:%v", vid)
+	return fmt.Sprintf("dm:data:full:{%v}", vid)
 }
 
 func GenHotDanmuCounterKey(vid int64) string {
